3974a4d91c258671261f7b6d03bd9299359e9537
Julian <mrjrhirst@gmail.com>
2023-10-01 17:33:58 +0100

    Ex 1: 2 tests running for capitalize()

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -6,7 +6,12 @@
 
 export function capitalize(word) {
 	if (word === undefined) throw new Error('word is required');
-	// Add your code here!
+
+	const firstLetter = word.charAt(0);
+	const firstLetterCapitalised = firstLetter.toUpperCase();
+	const remainingLetters = word.slice(1);
+
+	return firstLetterCapitalised + remainingLetters;
 }
 
 export function generateInitials(firstName, lastName) {

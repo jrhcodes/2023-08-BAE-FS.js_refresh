ee66c68461269d1c97281014e88c1ba9bcc8f1fe
Julian <mrjrhirst@gmail.com>
2023-10-04 20:27:34 +0100

    Ex4 Implemented with forEach

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -3,12 +3,11 @@ import { roundTo2DP } from "./jrhmathutils";
 export function findSmallNums(nums) {
 	if (!nums) throw new Error('nums is required');
 	const returnArray = [];
-	for (const i in nums) {
-		const value = nums[i];
+	nums.forEach(value => {
 		if (value < 1) {
 			returnArray.push(value);
 		}
-	}
+	} );
 	return returnArray;
 }
 
@@ -16,24 +15,22 @@ export function findNamesBeginningWith(names, char) {
 	if (!names) throw new Error('names is required');
 	if (!char) throw new Error('char is required');
 	const returnArray = [];
-	for (const i in names) {
-		const name = names[i];
+	names.forEach(name => {
         if (name[0] === char) {
             returnArray.push(name);
         }
-	}
+	});
 	return returnArray;
 }
 
 export function findVerbs(words) {
 	if (!words) throw new Error('words is required');
 	const returnArray = [];
-	for (const i in words) {
-		const word = words[i];
+	words.forEach(word => {
 		if (word.length > 3 && word.substring(0,3).toLowerCase() === 'to ') {
 			returnArray.push(word);
         }
-	}
+	});
     return returnArray;
 	
 }
@@ -41,31 +38,29 @@ export function findVerbs(words) {
 export function getIntegers(nums) {
 	if (!nums) throw new Error('nums is required');
 	const returnArray = [];
- 	for (const i in nums) {
-		const value = nums[i];
+	nums.forEach(value => {
 		if (value % 1.0 === 0) {
 			returnArray.push(value);
 		}
-	}
+	});
 	return returnArray;
 }
 
 export function getCities(users) {
 	if (!users) throw new Error('users is required');
 	const returnArray = [];
-	for (const i in users) {
-		returnArray.push(users[i].data.city.displayName);
-	}
+	users.forEach(user => {
+		returnArray.push(user.data.city.displayName);
+	});
 	return returnArray;
 }
 
 export function getSquareRoots(nums) {
 	if (!nums) throw new Error('nums is required');
 	const returnArray = [];
-	for (const i in nums) {
-        const value = nums[i];
+	nums.forEach(value => {
         returnArray.push(roundTo2DP(Math.sqrt(value)));
-    }
+    });
 	return returnArray;
 }
 
@@ -74,23 +69,20 @@ export function findSentencesContaining(sentences, str) {
 	if (!str) throw new Error('str is required');
 	const lowerCaseStr = str.toLowerCase();
 	const returnArray = [];
-	for( const i in sentences) {
-		const sentence = sentences[i].toLowerCase();
-        if (sentence.indexOf(lowerCaseStr) !== -1) {
-             returnArray.push(sentences[i]);
+	sentences.forEach(sentence => {
+		if (sentence.toLowerCase().indexOf(lowerCaseStr) !== -1) {
+             returnArray.push(sentence);
         }
-	}
+	});
 	return returnArray;
 }
 
 export function getLongestSides(triangles) {
 	if (!triangles) throw new Error('triangles is required');
 	const returnArray = [];
-	for( const i in triangles) {
-		const triangle = triangles[i];
+	triangles.forEach(triangle => {
         const longestSide = Math.max(...triangle);
         returnArray.push(longestSide);
-	}
+	});
 	return returnArray;
-
 }
